c63d3a540cefff213769c821c2ba8237fd939286
unknown <you@example.com>
2023-05-07 22:50:41 +0700

    update

--- lib/core/resources/assets.dart
+++ lib/core/resources/assets.dart
@@ -9,3 +9,7 @@ const meunu_profile = "assets/icons/menu_profile.svg";
 const meunu_setting = "assets/icons/menu_setting.svg";
 const icon_search = "assets/icons/Search.svg";
 const profile_pic = "assets/images/profile_pic.png";
+const documents = "assets/icons/Documents.svg";
+const media = "assets/icons/media.svg";
+const folder = "assets/icons/folder.svg";
+const unknown = "assets/icons/unknown.svg";
--- lib/screens/dash_board/dashboard_screen.dart
+++ lib/screens/dash_board/dashboard_screen.dart
@@ -2,8 +2,8 @@ import 'package:dashborad_flutter_responsive/constants.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
 import '../../core/resources/assets.dart';
+import 'components/chart.dart';
 import 'components/header.dart';
 
 class DashBoardScreen extends StatelessWidget {
@@ -21,6 +21,7 @@ class DashBoardScreen extends StatelessWidget {
                 height: defaultPadding,
               ),
               Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Expanded(
                     flex: 5,
@@ -36,13 +37,13 @@ class DashBoardScreen extends StatelessWidget {
                     flex: 2,
                     child: Container(
                       padding: const EdgeInsets.all(defaultPadding),
-                      height: 500,
                       decoration: const BoxDecoration(
                           color: secondaryColor,
                           borderRadius: BorderRadius.all(
                             Radius.circular(10),
                           )),
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
                             'Storage Details',
@@ -52,33 +53,32 @@ class DashBoardScreen extends StatelessWidget {
                             ),
                           ),
                           SizedBox(
-                            height: 200,
-                            child: Stack(
-                              children: [
-                                PieChart(PieChartData(
-                                    sectionsSpace: 0,
-                                    centerSpaceRadius: 70,
-                                    startDegreeOffset: -90,
-                                    sections: paiChartSelecttionDatas)),
-                                Positioned.fill(
-                                    child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      '29.1',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headline4
-                                          ?.copyWith(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w600,
-                                          ),
-                                    ),
-                                    Text('of 128GB')
-                                  ],
-                                ))
-                              ],
-                            ),
+                            height: defaultPadding,
+                          ),
+                          Chart(),
+                          StorageInfoCard(
+                            svgSrc: documents,
+                            title: 'Documents Files',
+                            amountOfFiles: '1.3GB',
+                            numOfFiles: 1328,
+                          ),
+                          StorageInfoCard(
+                            svgSrc: media,
+                            title: 'Media Files',
+                            amountOfFiles: '15.3GB',
+                            numOfFiles: 1328,
+                          ),
+                          StorageInfoCard(
+                            svgSrc: folder,
+                            title: 'Other Files',
+                            amountOfFiles: '1.3GB',
+                            numOfFiles: 1328,
+                          ),
+                          StorageInfoCard(
+                            svgSrc: unknown,
+                            title: 'Unkonwn',
+                            amountOfFiles: '15.3GB',
+                            numOfFiles: 140,
                           ),
                         ],
                       ),
@@ -92,35 +92,61 @@ class DashBoardScreen extends StatelessWidget {
   }
 }
 
-List<PieChartSectionData> paiChartSelecttionDatas = [
-  PieChartSectionData(
-    color: primaryColor,
-    value: 25,
-    showTitle: false,
-    radius: 25,
-  ),
-  PieChartSectionData(
-    color: const Color(0XFF26E5FF),
-    value: 20,
-    showTitle: false,
-    radius: 22,
-  ),
-  PieChartSectionData(
-    color: const Color(0XFFFFCF26),
-    value: 10,
-    showTitle: false,
-    radius: 19,
-  ),
-  PieChartSectionData(
-    color: const Color(0XFFEE2727),
-    value: 15,
-    showTitle: false,
-    radius: 16,
-  ),
-  PieChartSectionData(
-    color: primaryColor.withOpacity(0.1),
-    value: 26,
-    showTitle: false,
-    radius: 13,
-  ),
-];
+class StorageInfoCard extends StatelessWidget {
+  const StorageInfoCard({
+    super.key,
+    required this.title,
+    required this.svgSrc,
+    required this.amountOfFiles,
+    required this.numOfFiles,
+  });
+
+  final String title, svgSrc, amountOfFiles;
+  final int numOfFiles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(defaultPadding),
+      margin: const EdgeInsets.only(top: defaultPadding),
+      decoration: BoxDecoration(
+        border: Border.all(width: 2, color: primaryColor.withOpacity(0.15)),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(defaultPadding),
+        ),
+      ),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 20,
+            width: 20,
+            child: SvgPicture.asset(
+              svgSrc,
+            ),
+          ),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  '$numOfFiles Files',
+                  style: Theme.of(context).textTheme.caption?.copyWith(
+                        color: Colors.white70,
+                      ),
+                ),
+              ],
+            ),
+          )),
+          Text(amountOfFiles)
+        ],
+      ),
+    );
+  }
+}
